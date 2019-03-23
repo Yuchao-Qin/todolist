77427bcd00e9dc17a0099e92bc271b3a0f2a44c5
Yuchao-Qin <qinyuchao96@163.com>
2019-03-23 19:06:20 +0800

    'todolist'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,10 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <i-table :columns="columns1" :data="data1"></i-table>
+      <i-input :value.sync="value" placeholder="姓名..." v-model="entryname.name" style="width: 300px"></i-input>
+      <i-input :value.sync="value" placeholder="年龄..." v-model="entryname.age" style="width: 300px"></i-input>
+      <i-input :value.sync="value" placeholder="地址..." v-model="entryname.address" style="width: 300px"></i-input>
+      <i-button type="primary" @click="submit">提交</i-button>
   </div>
 </template>
 
@@ -35,6 +13,39 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      entryname:{
+        name:'',
+        age:'',
+        address:''
+      },
+      columns1: [
+                  {
+                      title: '姓名',
+                      key: 'name'
+                  },
+                  {
+                      title: '年龄',
+                      key: 'age'
+                  },
+                  {
+                      title: '地址',
+                      key: 'address'
+                  }
+                ],
+        data1: [
+        ]
+        }
+      },
+  methods: {
+    submit () {
+         this.data1.push({...this.entryname});
+         this.entryname.name = '';
+         this.entryname.address = '';
+         this.entryname.age = '';
+    }
   }
 }
 </script>
